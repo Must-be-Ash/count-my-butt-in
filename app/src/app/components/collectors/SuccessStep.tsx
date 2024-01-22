c43e937ff74bb68e068cc0e7bb95449944d079b0
hentony124 <dongphuongdang@gmail.com>
2024-01-21 17:11:51 -0800

    fix message

--- app/src/app/components/collectors/SuccessStep.tsx
+++ app/src/app/components/collectors/SuccessStep.tsx
@@ -11,37 +11,34 @@ export function SuccessStep() {
   const { campaign } = useCampaign(instance.campaignId);
   return (
     <div className="flex flex-col gap-2 w-full">
-    <div className="bg-white rounded-lg w-full p-4">
-      <div className="flex flex-row items-center justify-center">
-        {campaign && (
-          <NFTDisplayFull
-            networkId={nameToNetwork(campaign.networkId)}
-            tokenId={instance.mintedTokenId}
-            contractAddress={campaign.binderContract!}
-          />
-        )}
-        {
-          !campaign &&
-          <div className="flex flex-col justify-center h-[252px]">
-            <Loader color="black"  />
-          </div>
-        }
+      <div className="bg-white rounded-lg w-full p-4">
+        <div className="flex flex-row items-center justify-center">
+          {campaign && (
+            <NFTDisplayFull
+              networkId={nameToNetwork(campaign.networkId)}
+              tokenId={instance.mintedTokenId}
+              contractAddress={campaign.binderContract!}
+            />
+          )}
+          {!campaign && (
+            <div className="flex flex-col justify-center h-[252px]">
+              <Loader color="black" />
+            </div>
+          )}
+        </div>
+        <div className="text-center mt-4">
+          <h1 className="text-2xl font-bold">Congrats!</h1>
+          <p className="mt-2 text-gray-600">
+            Your token is waiting to be signed. You can head to your Binder
+            profile and wait for the autograph to reveal.
+          </p>
+        </div>
       </div>
-      <div className="text-center mt-4">
-        <h1 className="text-2xl font-bold">Congrats!</h1>
-        <p className="mt-2 text-gray-600">
-          Your token is waiting to be signed. You can head to your Binder
-          profile and wait for the autograph to reveal or you can let us notify
-          you via email.
-        </p>
-      </div>
-    </div>
       <Link
         href={`/asset/ethereum/${instance.contractAddress}/${instance.tokenId}`}
         className="w-full"
       >
-        <BinderButton className="w-full"
-        >See NFT</BinderButton>
+        <BinderButton className="w-full">See NFT</BinderButton>
       </Link>
     </div>
   );
