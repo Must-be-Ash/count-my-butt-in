d569cbb8450677ad69cb18c179e5aa1883702fcb
hentony124 <dongphuongdang@gmail.com>
2024-01-21 17:09:27 -0800

    color background fix

--- app/src/app/layouts/Main.tsx
+++ app/src/app/layouts/Main.tsx
@@ -2,15 +2,19 @@ import Footer from "@/app/components/Footer";
 import Nav from "@/components/Nav";
 import { ReactNode } from "react";
 
-const Main = ({children, hideNav = false}: {children: ReactNode; hideNav?: boolean}) => {
+const Main = ({
+  children,
+  hideNav = false,
+}: {
+  children: ReactNode;
+  hideNav?: boolean;
+}) => {
   return (
-    <main className="flex flex-col sm:max-w-[1024px] sm:mx-auto w-full h-screen p-3">
-        {!hideNav && <Nav />}
-        <div className="flex-1 flex flex-col items-center w-full">
-          {children}
-        </div>
+    <main className="flex flex-col sm:mx-auto w-full h-screen p-3">
+      {!hideNav && <Nav />}
+      <div className="flex-1 flex flex-col items-center w-full">{children}</div>
     </main>
-  )
-}
+  );
+};
 
 export default Main;
