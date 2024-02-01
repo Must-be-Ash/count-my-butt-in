40815503158dc662d5deefbefdbd8ea080ef0f53
Angela Gilhotra <angelagilhotra@gmail.com>
2024-02-01 12:45:49 -0800

    collector landing

--- app/src/utils/getArtistFromHost.ts
+++ app/src/utils/getArtistFromHost.ts
@@ -14,6 +14,7 @@ export default async function getArtistCampaignFromHost(host: string | null) {
   }
   return { campaign: campaign[0], artist: {
     nickname: artist.nickname,
-    twitterUsername: artist.twitterUsername
+    twitterUsername: artist.twitterUsername,
+    imageUrl: artist.imageUrl
   } }
 }
