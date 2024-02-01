44ed1b2dd3347bb7b87cafc4d37b64ce86ab1b47
Angela Gilhotra <angelagilhotra@gmail.com>
2024-02-01 13:54:56 -0800

    landing fixes

--- app/src/app/components/CampaignLanding.tsx
+++ app/src/app/components/CampaignLanding.tsx
@@ -1,9 +1,11 @@
+"use client"
 import { Campaign, User } from "@prisma/client";
 import Image from "next/image";
 import BinderButton from "./BinderButton";
 import Link from "next/link";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
-export default async function CampaignLanding({
+export default function CampaignLanding({
     artist,
     campaign
   }:{
@@ -11,53 +13,64 @@ export default async function CampaignLanding({
     artist: {nickname: string, twitterUsername: string, imageUrl?: string}
   }
 ){
+  const desktop = "(min-width: 768px)"
+  const isDesktop = useMediaQuery(desktop)
   return (
     <div className="grid sm:grid-cols-2 h-screen w-screen font-sans overflow-hidden">
-      <div className="w-full p-6 shadow-2xl h-full">
-        <div className="flex flex-col items-center justify-between h-full">
-          <div className="flex flex-row justify-between items-center w-full">
-            <a className="cursor-pointer" href="https://signed.gg" target="_blank">
-              <Image
-                src="/signedgg.png"
+      <div className="shadow-2xl h-full bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-stone-300 via-indigo-300 to-emerald-400 sm:bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] sm:from-[#101010] sm:via-[#121212] sm:to-black">
+        <div className="w-full h-full p-6 text-slate-800 sm:text-slate-200">
+          <div className="flex flex-col items-center justify-between h-full">
+            <div className="flex flex-row justify-between items-center w-full">
+              <a className="cursor-pointer" href="https://signed.gg" target="_blank">
+                {isDesktop ? <Image
+                  src="/signedgg.png"
+                  alt="signed-logo"
+                  className="w-10 h-10"
+                  width="250"
+                  height="250"
+                /> : <Image
+                src="/signedgg-dark.png"
                 alt="signed-logo"
                 className="w-10 h-10"
                 width="250"
                 height="250"
-              />
-            </a>
-            <a className="cursor-pointer" href={`https://x.com/${artist.twitterUsername}`} target="_blank">@{artist.twitterUsername}</a>
-          </div>
-          <div className="w-full flex flex-col gap-3 items-center justify-center h-full">
-            <div className="font-sans font-black uppercase text-lg">
-            {artist.nickname}
-            </div>
-            <div className="text-3xl font-title italic">
-              Autograph Session
-            </div>
-            {artist.imageUrl && <Image
-              src={artist.imageUrl}
-              alt="profile picture"
-              className="rounded-full w-40 h-40"
-              width={98}
-              height={98}
-            />}
-            <div className="pt-4">
-              <a href={`/collector/${campaign.campaignId}`} target="_blank">
-                <BinderButton className="cursor-pointer">
-                  Request Signature
-                </BinderButton>
+              /> }
               </a>
+              <a className="cursor-pointer uppercase font-sans font-black text-sm" href={`https://x.com/${artist.twitterUsername}`} target="_blank">@{artist.twitterUsername}</a>
             </div>
+            <div className="w-full flex flex-col gap-6 items-center justify-center h-full">
+              <div className="flex flex-col text-3xl pb-3 items-center justify-center">
+                <div className="font-sans font-black uppercase">
+                {artist.nickname}
+                </div>
+                <div className="font-title italic">
+                  Autograph Session
+                </div>
+              </div>
+              {artist.imageUrl && <Image
+                src={artist.imageUrl}
+                alt="profile picture"
+                className="rounded-full w-40 h-40 shadow-xl"
+                width={98}
+                height={98}
+              />}
+              <div className="pt-4">
+                <a href={`/collector/${campaign.campaignId}`} target="_blank">
+                  <BinderButton className="cursor-pointer dark:bg-slate-900 dark:text-white sm:dark:bg-slate-300 sm:dark:text-slate-900">
+                    Request Signature
+                  </BinderButton>
+                </a>
+              </div>
+            </div>
+            <a href="https://binder.studio" target="_blank">
+              <div className="flex flex-row gap-1 items-center font-thin cursor-pointer text-sm ">
+                Powered by
+                <span className="font-title italic">
+                  Binder<span className="text-green-300">.</span>Studio
+                </span>
+              </div>
+            </a>
           </div>
-          <a href="https://binder.studio" target="_blank">
-            <div className="flex flex-row gap-1 items-center font-thin cursor-pointer text-sm">
-              Powered by
-              <span className="font-title italic">
-                Binder<span className="text-green-300">.</span>Studio
-              </span>
-            </div>
-          </a>
-
         </div>
       </div>
       <div className="sm:block hidden">
