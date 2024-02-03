344f63b489b0cc131f0b48210f8ddd2d2277c197
hentony124 <dongphuongdang@gmail.com>
2024-02-03 11:35:55 -0800

    add error message

--- app/src/app/export/page.tsx
+++ app/src/app/export/page.tsx
@@ -7,11 +7,12 @@ import { useState } from "react";
 import LogoutButton from "@/app/components/LogoutButton";
 import { useAuthentication } from "@/hooks/useAuthentication";
 import { usePrivy } from "@privy-io/react-auth";
+import ErrorDisplay from "../components/ErrorDisplay";
 
 export default function HeyYo() {
   const { exportWallet } = usePrivy();
   const { authenticated, user, wallets } = useAuthentication();
-  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
   return (
     <main className="flex  flex-col items-center justify-between p-24">
@@ -19,11 +20,16 @@ export default function HeyYo() {
 
       {authenticated && (
         <div className="pt-4 pb-4">
-          <BinderButton onClick={exportWallet}>Export Wallet</BinderButton>
+          <BinderButton
+            onClick={() => exportWallet().catch((e) => setError(e))}
+          >
+            Export Wallet
+          </BinderButton>
         </div>
       )}
 
       {authenticated && <LogoutButton size="lg" withText />}
+      {error && <ErrorDisplay error={error} />}
       {success && <div className="text-white text-xl">ETH SENT! 🎉</div>}
     </main>
   );
