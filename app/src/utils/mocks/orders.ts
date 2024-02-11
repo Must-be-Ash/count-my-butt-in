496ee24e4423a7256e571389ad5bfaca7a739745
hentony124 <dongphuongdang@gmail.com>
2024-02-10 23:30:52 -0800

    fix order

--- app/src/utils/mocks/orders.ts
+++ app/src/utils/mocks/orders.ts
@@ -1,52 +1,54 @@
+import { Order } from "@prisma/client";
+
 const orders: Array<Order> = [
   {
     collectionTitle: "Doodle",
     tokenId: "1234",
     image: "https://i.imgur.com/NA9Oanc.jpg",
     autograph: undefined, // arweave link
-    status: "PENDING"
+    status: "PENDING",
   },
   {
     collectionTitle: "Doodle",
     tokenId: "1345",
     image: "https://i.imgur.com/NA9Oanc.jpg",
     autograph: "", // arweave link
-    status: "AUTOGRAPHED"
+    status: "AUTOGRAPHED",
   },
   {
     collectionTitle: "Doodle",
     tokenId: "1345",
     image: "https://i.imgur.com/NA9Oanc.jpg",
     autograph: "", // arweave link
-    status: "AUTOGRAPHED"
+    status: "AUTOGRAPHED",
   },
   {
     collectionTitle: "Doodle",
     tokenId: "1345",
     image: "https://i.imgur.com/NA9Oanc.jpg",
     autograph: "", // arweave link
-    status: "AUTOGRAPHED"
+    status: "AUTOGRAPHED",
   },
   {
     collectionTitle: "Doodle",
     tokenId: "1345",
     image: "https://i.imgur.com/NA9Oanc.jpg",
     autograph: "", // arweave link
-    status: "AUTOGRAPHED"
+    status: "AUTOGRAPHED",
   },
   {
     collectionTitle: "Doodle",
     tokenId: "1345",
     image: "https://i.imgur.com/NA9Oanc.jpg",
     autograph: "", // arweave link
-    status: "AUTOGRAPHED"
+    status: "AUTOGRAPHED",
   },
   {
     collectionTitle: "Doodle",
     tokenId: "1345",
     image: "https://i.imgur.com/NA9Oanc.jpg",
     autograph: "", // arweave link
-    status: "AUTOGRAPHED"
-  }
-]
+    status: "AUTOGRAPHED",
+  },
+];
 export default orders;
