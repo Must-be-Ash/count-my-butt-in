c08c0da02b69f14f717dd66bd65127fcbf07a16c
hentony124 <dongphuongdang@gmail.com>
2024-02-13 16:52:27 -0800

    add new contract

--- app/src/utils/common.ts
+++ app/src/utils/common.ts
@@ -161,7 +161,7 @@ export const MINT_FEE = 0.01;
 //   [11155111]: "0xc9f8daf5a1d54f6f2b6cfb8049c10111ba8d69a1",
 // };
 
-export const BINDER_DROP_TOKEN = "0xfbfff11455d562ec156d54df5562c1bc680ebc11";
+export const BINDER_DROP_TOKEN = "0x9d5ab5325cfd092f096e5b0f6a19fe00a42149ea";
 
 export const childrenNetworkIds = [1, 137, 8453, 11155111, 10];
 
@@ -213,5 +213,18 @@ export const PAYMENT_WALLET: Record<number, string> = {
   [11155111]: "0xb3485416bb361093188529bacd5551ac420013ab",
 };
 
-export const dayNames = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"]
-export const monthNames = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+export const dayNames = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
+export const monthNames = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
