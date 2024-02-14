112ed50eab0ed9a63e7bd5d0b06502364c03126c
hentony124 <dongphuongdang@gmail.com>
2024-02-13 16:53:41 -0800

    switch to mainnet

--- app/src/app/components/collectors/EnterWalletStep.tsx
+++ app/src/app/components/collectors/EnterWalletStep.tsx
@@ -26,55 +26,53 @@ export default function EnterWalletStep() {
   }
 
   return (
-      <div className="flex flex-col gap-4 grow w-full justify-start">
-        <h1>Enter a wallet address</h1>
-        <div>
-          Enter the wallet address that contains the NFT you want signed
-        </div>
-        <div className="flex flex-row w-full">
-          <div className="grow">
-            <label htmlFor="search" className="sr-only">
-              enter address
-            </label>
-            <input
-              type="address"
-              name="address"
-              id="address"
-              className="block w-full pl-4 py-1 rounded-md border-0 text-ellipsis text-white shadow-sm bg-black ring-1 ring-inset ring-neutral-400 placeholder:text-white placeholder:opacity-40 focus:ring-2 focus:ring-inset focus:ring-neutral-700 sm:text-sm sm:leading-6"
-              placeholder={
-                selectedAddress
-                  ? selectedAddress
-                  : "Enter a wallet address or ENS name"
-              }
-              onChange={(e) => selectAddress(e.target.value)}
-            />
-          </div>
-        </div>
-        <ErrorDisplay error={error} />
-
-        {selectedAddress && (
-          <NFTFeedProfile
-            searchWord={searhWord}
-            addresses={[selectedAddress]}
-            networkId={11155111}
-            onClickCallback={(
-              networkId: string,
-              contractAddress: string,
-              tokenId: string,
-              imageUrl: string,
-              name: string
-            ) => {
-              setInstance({
-                nftNetworkId: parseInt(networkId),
-                contractAddress,
-                tokenId,
-                nftUrl: imageUrl,
-                name,
-              });
-              setCurrentStepIndex(1);
-            }}
+    <div className="flex flex-col gap-4 grow w-full justify-start">
+      <h1>Enter a wallet address</h1>
+      <div>Enter the wallet address that contains the NFT you want signed</div>
+      <div className="flex flex-row w-full">
+        <div className="grow">
+          <label htmlFor="search" className="sr-only">
+            enter address
+          </label>
+          <input
+            type="address"
+            name="address"
+            id="address"
+            className="block w-full pl-4 py-1 rounded-md border-0 text-ellipsis text-white shadow-sm bg-black ring-1 ring-inset ring-neutral-400 placeholder:text-white placeholder:opacity-40 focus:ring-2 focus:ring-inset focus:ring-neutral-700 sm:text-sm sm:leading-6"
+            placeholder={
+              selectedAddress
+                ? selectedAddress
+                : "Enter a wallet address or ENS name"
+            }
+            onChange={(e) => selectAddress(e.target.value)}
           />
-        )}
+        </div>
       </div>
+      <ErrorDisplay error={error} />
+
+      {selectedAddress && (
+        <NFTFeedProfile
+          searchWord={searhWord}
+          addresses={[selectedAddress]}
+          networkId={1}
+          onClickCallback={(
+            networkId: string,
+            contractAddress: string,
+            tokenId: string,
+            imageUrl: string,
+            name: string
+          ) => {
+            setInstance({
+              nftNetworkId: parseInt(networkId),
+              contractAddress,
+              tokenId,
+              nftUrl: imageUrl,
+              name,
+            });
+            setCurrentStepIndex(1);
+          }}
+        />
+      )}
+    </div>
   );
 }
