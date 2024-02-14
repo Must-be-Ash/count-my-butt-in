2c7007408a747f019b2edac9a9c081f9e3f5668a
hentony124 <dongphuongdang@gmail.com>
2024-02-14 13:52:41 -0800

    fix mobile display on view all campaigns

--- app/src/components/Nav.tsx
+++ app/src/components/Nav.tsx
@@ -15,19 +15,21 @@ import { usePathname } from "next/navigation";
 
 const ViewAllCampaignsButton = () => {
   return (
-    <Link href={`/dashboard/campaigns`} className="flex flex-row gap-2 px-4 py-2 text-sm border-dashed border-2 border-white rounded-lg cursor-pointer justify-end">
-    <Notebook className="w-5 h-5" />
-    <span>
-      View all Campaigns
-    </span>
-  </Link>
-  )
-}
+    <Link
+      href={`/dashboard/campaigns`}
+      className="flex flex-row gap-2 px-4 py-2  border-dashed border-2 border-white rounded-lg cursor-pointer justify-end sm:text-sm text-xs"
+    >
+      <Notebook className="w-5 h-5" />
+      <span className="hidden sm:block">View Campaigns</span>
+    </Link>
+  );
+};
 
 const LoginOrUserWidget = () => {
   const { user } = useAuthentication();
   const path = usePathname();
-  const isOnDashboard = path.includes("/dashboard") && path !== ("/dashboard/campaigns");
+  const isOnDashboard =
+    path.includes("/dashboard") && path !== "/dashboard/campaigns";
 
   if (!user) {
     return <LoginButton />;
@@ -35,7 +37,7 @@ const LoginOrUserWidget = () => {
   return (
     <div className="flex flex-row gap-2 items-center w-full justify-between pb-6">
       <UserInfo />
-        <div className="flex flex-row gap-2">
+      <div className="flex flex-row gap-2">
         {isOnDashboard && <ViewAllCampaignsButton />}
         <LogoutButton size="sm" />
       </div>
