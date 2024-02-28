c4723ec4bd908e4606dfe0746ca0815fcb6b3506
hentony124 <dongphuongdang@gmail.com>
2024-02-28 13:25:59 -0800

    bump fund amount to 0.001

--- app/src/app/api/fund/route.tsx
+++ app/src/app/api/fund/route.tsx
@@ -30,7 +30,7 @@ export async function POST(request: NextRequest) {
     receiverAddress
   );
   const balanceInEther = formatEther(balance);
-  if (parseFloat(balanceInEther) > 0.00094) {
+  if (parseFloat(balanceInEther) > 0.001) {
     return NextResponse.json({
       success: true,
       message: "Balance is sufficient",
@@ -43,7 +43,7 @@ export async function POST(request: NextRequest) {
   // Send a transaction
   await wallet.sendTransaction({
     to: receiverAddress,
-    value: parseUnits("0.00094", "ether"),
+    value: parseUnits("0.001", "ether"),
   });
 
   await updateUser(userId, {
